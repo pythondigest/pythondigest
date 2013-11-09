59765c7c2df4f3cc2c7694d138b2d45ccaf95c7c
alrusdi <alrusdi@gmail.com>
2013-11-09 22:45:41 +0600

    Нескучный вид админки за счет django-admin-bootstrap

--- conf/settings.py
+++ conf/settings.py
@@ -134,6 +134,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap_admin',
     'django.contrib.admin',
     'south',
     'sorl.thumbnail',
