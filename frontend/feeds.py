0409b95122da5c82fff5dc040a4a5c2f1b40bb24
Kanarev Artem <kanarev.a@tigrio.ru>
2013-11-11 19:30:19 +0700

    add rss feed

---
+++ frontend/feeds.py
@@ -0,0 +1,28 @@
+# coding=utf-8
+from django.contrib.syndication.views import Feed
+from digest.models import Item
+
+
+class LatestEntriesFeed(Feed):
+    """
+    Лента РСС для новостей
+    """
+    title = u"Дайджест новостей о python"
+    link = "/feed/"
+    description = u"""Новости собираются с мира по нитке на совершенно безвозмезной основе. Ты легко можешь
+    посодействовать проекту добавив ссылку на интересную новость, статью, интервью или проект о python. А еще можно
+    форкнуть код этого проекта на Github и помочь в развитии его функциональности."""
+
+    @staticmethod
+    def items():
+        return Item.objects.order_by('-created_at')[:10]
+
+    def item_title(self, item):
+        return item.title
+
+    def item_description(self, item):
+        return item.description
+
+    # item_link is only needed if NewsItem has no get_absolute_url method.
+    def item_link(self, item):
+        return item.link
