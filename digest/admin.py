f3e96ed9e2869506789fc312757b1deb1b9b6cb5
evgen <evgen@funtoo>
2013-11-13 13:04:42 +0600

    hot fix

--- digest/admin.py
+++ digest/admin.py
@@ -5,12 +5,10 @@ from django import forms
 from digest.models import Issue, Section, Item, Resource
 
 class IssueAdmin(admin.ModelAdmin):
-    exclude = ('version',)
     pass
 admin.site.register(Issue, IssueAdmin)
 
 class SectionAdmin(admin.ModelAdmin):
-    exclude = ('version',)
     pass
 admin.site.register(Section, SectionAdmin)
 
@@ -19,10 +17,8 @@ class ItemAdmin(admin.ModelAdmin):
     list_filter = ('status', 'issue',)
     list_display = ('title', 'status', 'issue', 'related_to_date')
     radio_fields = {'language': admin.HORIZONTAL}
-    exclude = ('version',)
 admin.site.register(Item, ItemAdmin)
 
 class ResourceAdmin(admin.ModelAdmin):
-    exclude = ('version',)
     pass
 admin.site.register(Resource, ResourceAdmin)
