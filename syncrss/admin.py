07b5c4e959d51c485cc62fd1dd369e0df2179fce
axcel <axce1.github@gmail.com>
2014-06-05 07:40:52 +0600

    ugly move rss news to feed

--- syncrss/admin.py
+++ syncrss/admin.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
 from syncrss.models import RawItem, ResourceRSS
+from digest.models import Item
 
 class ResourceRSSAdmin(admin.ModelAdmin):
     list_display = ('title', 'language', 'status', 'sync_date')
@@ -9,8 +10,22 @@ class ResourceRSSAdmin(admin.ModelAdmin):
 admin.site.register(ResourceRSS, ResourceRSSAdmin)
 
 
+def make_published(modeladmin, request, queryset):
+    #queryset.update(status='active')
+    #print(queryset)
+    for i in queryset:
+        # soooo... ugly
+        obj = Item(title=i.title, \
+            description=i.description, \
+            link=i.link,status=i.status, \
+            related_to_date=i.related_to_date, language=i.language, \
+            version=i.version)
+        obj.save()
+make_published.short_description = "Перенести выделенные новости в ленту"
+
 class RawItemAdmin(admin.ModelAdmin):
-    list_display = ('title', 'link', 'related_to_date')
-    list_filter = ('resource_rss', 'related_to_date')
+    list_display = ('title', 'link', 'related_to_date', 'status')
+    list_filter = ('resource_rss', 'related_to_date', 'status')
     search_fields = ['title']
+    actions = [make_published]
 admin.site.register(RawItem, RawItemAdmin)
