2803823d16cdfee9e23ea91cb4a098e3c596867d
Sapronov Alexander <sapronov.alexander92@gmail.com>
2015-08-14 09:55:40 +0600

    Добавил sitemap

--- conf/settings.py
+++ conf/settings.py
@@ -22,6 +22,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sitemaps',
 
     'bootstrap_admin',
 
--- conf/urls.py
+++ conf/urls.py
@@ -1,12 +1,34 @@
 from django.conf import settings
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+from django.contrib.sitemaps.views import sitemap
+from django.contrib.sitemaps import Sitemap
+from digest.models import Issue, Item
+
 admin.autodiscover()
+
+class IssueSitemap(Sitemap):
+
+    changefreq = "never"
+    priority = 0.5
+    i18n = True
+
+    def items(self):
+        return Issue.objects.filter(status='active')
+
+    def lastmod(self, obj):
+        return obj.published_at
+
+
+
+sitemaps = {
+    'issue': IssueSitemap,
+}
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
     url(r'', include('frontend.urls', namespace='frontend')),
+    url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap')
 )
