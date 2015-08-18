4575c39523a9939eb6fca3cf9729b8d2e09ddff2
Sapronov Alexander <sapronov.alexander92@gmail.com>
2015-08-18 14:24:38 +0600

    Добавил фаб файлы

---
+++ fabfile/fabfile.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# 
+import os
+
+from fabric.api import *
+
+PROJECT_FOLDER = '/home/pythondigest/pythondigest.ru/'
+REPO_FOLDER = os.path.join(PROJECT_FOLDER, 'repo')
+ENV_FOLDER = os.path.join(PROJECT_FOLDER, 'env')
+ENV_PATH = os.path.join(ENV_FOLDER, 'bin/activate')
+
+
+def pull():
+    with cd(REPO_FOLDER):
+        run('git pull')
+
+
+def deploy():
+    pull()
+    migrate()
+    static()
+    restart()
+
+
+def restart():
+    sudo('service uwsgi restart')
+
+
+def migrate():
+    with prefix('source %s' % ENV_PATH):
+        with cd(REPO_FOLDER):
+            run("python manage.py migrate --noinput")
+
+
+def static():
+    with prefix('source %s' % ENV_PATH):
+        with cd(REPO_FOLDER):
+            run("python manage.py collectstatic --noinput")
