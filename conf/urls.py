4c6012ddbb91d11c40adc1243b6000729f04ef99
Sapronov Alexander <sapronov.alexander92@gmail.com>
2015-08-20 08:41:57 +0600

    Edit: Pep8

--- conf/urls.py
+++ conf/urls.py
@@ -8,8 +8,7 @@ admin.autodiscover()
 urlpatterns = patterns('', url(r'^admin/', include(admin.site.urls)),
                        url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
                            {'document_root': settings.MEDIA_ROOT}),
-                       url(r'', include('frontend.urls',
-                                        namespace='frontend')),
+                       url(r'', include('frontend.urls', namespace='frontend')),
                        url(r"^account/", include('account.urls')),
                        url(r'', include('social.apps.django_app.urls',
                                         namespace='social')))
--- frontend/urls.py
+++ frontend/urls.py
@@ -9,34 +9,27 @@ from .feeds import AllEntriesFeed, IssuesFeed, ItemArticleFeed, \
     ItemVideoFeed, RussianEntriesFeed, TwitterEntriesFeed
 
 urlpatterns = patterns(
-    '', url(r'^view/(?P<pk>[0-9]+)/$', ItemView.as_view(),
-            name='item'), url(r'^add/$', AddNews.as_view(),
-                              name='addnews'),
-    url(r'^feed/$', NewsList.as_view(),
-        name='feed'), url(r'^rss/$', AllEntriesFeed(),
-                          name='rss'), url(r'^rss/direct/$', AllEntriesFeed(),
-                                           name='rss_direct'),
-    url(r'^rss/twitter/$', TwitterEntriesFeed(),
-        name='rss_twitter'), url(r'^rss/ru/$', RussianEntriesFeed(),
-                                 name='russian_rss'),
-    url(r'^rss/issues/$', IssuesFeed(),
-        name='issues_rss'),  # hindi
+    '',
+    url(r'^view/(?P<pk>[0-9]+)/$', ItemView.as_view(), name='item'),
+    url(r'^add/$', AddNews.as_view(),  name='addnews'),
+    url(r'^feed/$', NewsList.as_view(), name='feed'),
+    url(r'^rss/$', AllEntriesFeed(), name='rss'),
+    url(r'^rss/direct/$', AllEntriesFeed(), name='rss_direct'),
+    url(r'^rss/twitter/$', TwitterEntriesFeed(), name='rss_twitter'),
+    url(r'^rss/ru/$', RussianEntriesFeed(), name='russian_rss'),
+    url(r'^rss/issues/$', IssuesFeed(), name='issues_rss'),  # hindi
     # solution
-    url(r'^rss/video/$', ItemVideoFeed(),
-        name='video_rss'
-        ), url(r'^rss/recommend/$', ItemRecommendFeed(),
-               name='recommend_rss'), url(r'^rss/news/$', ItemNewsFeed(),
-                                          name='news_rss'),
-    url(r'^rss/bookdoc/$', ItemBookDocFeed(),
-        name='book_doc_rss'), url(r'^rss/event/$', ItemEventFeed(),
-                                  name='event_rss'),
-    url(r'^rss/article/$', ItemArticleFeed(),
-        name='article_rss'), url(r'^rss/release/$', ItemReleaseFeed(),
-                                 name='release_rss'),
-    url(r'^rss/packages/$', ItemPackagesFeed(),
-        name='packages_rss'), url(r'^issues/$', IssuesList.as_view(),
-                                  name='issues'),
-    url(r'^issue/(?P<pk>[0-9]+)/$', IssueView.as_view(),
-        name='issue_view'), url(r'^sitemap\.xml$', Sitemap.as_view(),
-                                name='sitemap'), url(r'^$', Index.as_view(),
-                                                     name='home'), )
+    url(r'^rss/video/$', ItemVideoFeed(), name='video_rss'),
+    url(r'^rss/recommend/$', ItemRecommendFeed(), name='recommend_rss'),
+    url(r'^rss/news/$', ItemNewsFeed(), name='news_rss'),
+    url(r'^rss/bookdoc/$', ItemBookDocFeed(), name='book_doc_rss'),
+    url(r'^rss/event/$', ItemEventFeed(), name='event_rss'),
+    url(r'^rss/article/$', ItemArticleFeed(), name='article_rss'),
+    url(r'^rss/release/$', ItemReleaseFeed(), name='release_rss'),
+    url(r'^rss/packages/$', ItemPackagesFeed(), name='packages_rss'),
+    url(r'^issues/$', IssuesList.as_view(), name='issues'),
+    url(r'^issue/(?P<pk>[0-9]+)/$', IssueView.as_view(),  name='issue_view'),
+    url(r'^sitemap\.xml$', Sitemap.as_view(), name='sitemap'),
+    url(r'^$', Index.as_view(), name='home'),
+
+)
