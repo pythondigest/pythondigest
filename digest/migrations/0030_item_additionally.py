15944c815902b51fe445059bb828b43349b73ab5
Sapronov Alexander <sapronov.alexander92@gmail.com>
2015-09-01 13:40:06 +0600

    ADD: теперь для новостей, которые относятся к разделу Видео и которые не ссылаются напрямую на youtube, будет применятся попытка вытащить ссылку на видео.

---
+++ digest/migrations/0030_item_additionally.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('digest', '0029_auto_20150825_1202'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='item',
+            name='additionally',
+            field=models.CharField(verbose_name='Дополнительно', null=True, max_length=255, blank=True),
+        ),
+    ]
