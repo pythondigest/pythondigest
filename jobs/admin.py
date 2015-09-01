4ce89036ea6995bb057fe0fd1d1a967b8bd77f36
Sapronov Alexander <sapronov.alexander92@gmail.com>
2015-09-01 14:31:35 +0600

    ADD: добавил модель JobsFeed

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,3 +1,22 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
 
-# Register your models here.
+from digest.admin import link_html
+from jobs.models import JobFeed
+
+
+class JobFeedAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'link_html',
+        'incl',
+        'excl',
+        'in_edit',
+    )
+
+    link_html = lambda s, obj: link_html(obj)
+    link_html.allow_tags = True
+    link_html.short_description = u"Ссылка"
+
+
+admin.site.register(JobFeed, JobFeedAdmin)
--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,42 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 
-# Create your models here.
+
+class JobFeed(models.Model):
+    """RSS - источники импорта вакансий."""
+    name = models.CharField(
+        max_length=255,
+        verbose_name=u'Название источника',
+    )
+
+    link = models.URLField(
+        max_length=255, verbose_name=u'Ссылка',
+    )
+
+    incl = models.CharField(
+        max_length=255,
+        verbose_name=u'Обязательное содержание',
+        help_text='Условие отбора новостей <br /> \
+                   Включение вида [text] <br /> \
+                   Включение при выводе будет удалено',
+        null=True,
+        blank=True,
+    )
+
+    excl = models.TextField(
+        verbose_name=u'Список исключений',
+        help_text='Список источников подлежащих исключению через ", "',
+        null=True,
+        blank=True,
+    )
+    in_edit = models.BooleanField(
+        verbose_name=u'На тестировании',
+        default=False,
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = u'Источник импорта вакансий'
+        verbose_name_plural = u'Источники импорта вакансий'
