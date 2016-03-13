0b5b992723cf66d2dae36c6a0df742af172c9614
Alexander Sapronov <sapronov.alexander92@gmail.com>
2016-03-13 10:42:14 +0600

    ADD: добавил комманду create_dataset_ids для создания датасета на основе id'шников

--- digest/management/commands/create_dataset.py
+++ digest/management/commands/create_dataset.py
@@ -1,55 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-import json
-import os
 from datetime import datetime, timedelta, date
 
-import grequests
-from django.conf import settings
 from django.core.management.base import BaseCommand
 
+from digest.management.commands.create_dataset_ids import create_dataset
 from digest.models import Item, get_start_end_of_week
-
-
-def check_exist_link(data, item):
-    for info in data.get('links'):
-        if info['link'] == item.link:
-            return True
-    else:
-        return False
-
-
-def create_dataset(start_date, end_date, name):
-    out_filepath = os.path.join(settings.DATASET_FOLDER, name)
-
-    if os.path.exists(out_filepath):
-        with open(out_filepath, 'r') as fio:
-            try:
-                data = json.load(fio)
-            except ValueError as e:
-                print(e)
-                data = {'links': []}
-    else:
-        data = {'links': []}
-
-    items = {x.link: x for x in Item.objects.filter(
-        related_to_date__range=[
-            start_date,
-            end_date
-        ])[:5] if not check_exist_link(data, x)}
-
-    rs = (grequests.get(u) for u in items.keys())
-    resps = grequests.map(rs)
-
-    for res in resps:
-        data['links'].append(items[res.url].get_data4cls(status=True, text=items[res.url].get_text(res.text)))
-
-    if not os.path.exists(os.path.dirname(out_filepath)):
-        os.makedirs(os.path.dirname(out_filepath))
-
-    with open(out_filepath, 'w') as fio:
-        json.dump(data, fio)
 
 
 class Command(BaseCommand):
@@ -71,4 +28,11 @@ class Command(BaseCommand):
 
         start, end = get_start_end_of_week(data)
         name = 'data_{}_{}.json'.format(options['year'], options['week'])
-        create_dataset(start, end, name)
+
+        items = Item.objects.filter(
+            related_to_date__range=[
+                start,
+                end
+            ])
+
+        create_dataset(items, name)
---
+++ digest/management/commands/create_dataset_ids.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import json
+import os
+
+import grequests
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+from digest.models import Item
+
+
+def check_exist_link(data, item):
+    for info in data.get('links'):
+        if info['link'] == item.link:
+            return True
+    else:
+        return False
+
+
+def create_dataset(queryset_items, name):
+    out_filepath = os.path.join(settings.DATASET_FOLDER, name)
+
+    if os.path.exists(out_filepath):
+        with open(out_filepath, 'r') as fio:
+            try:
+                data = json.load(fio)
+            except ValueError as e:
+                print(e)
+                data = {'links': []}
+    else:
+        data = {'links': []}
+
+    items = {x.link: x for x in queryset_items if not check_exist_link(data, x)}
+
+    rs = (grequests.get(u) for u in items.keys())
+    resps = grequests.map(rs)
+
+    for res in resps:
+        data['links'].append(items[res.url].get_data4cls(status=True, text=items[res.url].get_text(res.text)))
+
+    if not os.path.exists(os.path.dirname(out_filepath)):
+        os.makedirs(os.path.dirname(out_filepath))
+
+    with open(out_filepath, 'w') as fio:
+        json.dump(data, fio)
+
+
+class Command(BaseCommand):
+    help = u'Create dataset'
+
+    def add_arguments(self, parser):
+        parser.add_argument('count', type=int)  # сколько элементов в части
+        parser.add_argument('part', type=int)  # какая часть
+
+    def handle(self, *args, **options):
+        """
+        Основной метод - точка входа
+        """
+        count = options['count']
+        part = options['part']
+        name = 'data_{}_{}.json'.format(options['count'], options['part'])
+
+        items = Item.objects.all().order_by('id')[part * count: (part + 1) * count]
+
+        create_dataset(items, name)
