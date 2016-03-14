427e40b3f6b891e4395583c1f060eb8d3d5b4731
Alexander Sapronov <sapronov.alexander92@gmail.com>
2016-03-14 12:34:04 +0600

    ADD: добавил команду для публикации дайджеста из консоли

---
+++ digest/management/commands/post_issue_in_vk.py
@@ -0,0 +1,28 @@
+# -*- encoding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from django.core.management.base import BaseCommand
+
+from digest.models import Issue
+from digest.pub_digest import pub_to_all
+
+
+class Command(BaseCommand):
+    args = 'no arguments!'
+    help = u'News import from external resources'
+
+    def add_arguments(self, parser):
+        parser.add_argument('issue', type=int)
+
+    def handle(self, *args, **options):
+        """
+        Основной метод - точка входа
+        """
+        issue = Issue.objects.get(pk=options['issue'])
+        site = 'http://pythondigest.ru'
+
+        pub_to_all(
+            issue.announcement,
+            '{}{}'.format(site, issue.link),
+            '{}{}'.format(site, issue.image.url if issue.image else ''))
