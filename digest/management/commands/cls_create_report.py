2bba17532745601585b028bcf10365f5cb8a3784
Alexander Sapronov <sapronov.alexander92@gmail.com>
2016-05-03 22:24:45 +0600

    ADD: добавлены команды cls_* для работы с датасетом для классификатора

---
+++ digest/management/commands/cls_create_report.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import csv
+import json
+import os
+
+import requests
+import simplejson
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+from digest.management.commands.cls_split_dataset import load_data_from_folder
+
+
+class Command(BaseCommand):
+    help = u'Create dataset'
+
+    def add_arguments(self, parser):
+        parser.add_argument('dataset_test_folder', type=str)
+        parser.add_argument('out_path', type=str)
+
+    def handle(self, *args, **options):
+        """
+        Основной метод - точка входа
+        """
+        items = load_data_from_folder(options['dataset_test_folder'])
+
+        part_size = 100
+        cur_part = 0
+        url = "{0}/{1}".format(settings.CLS_URL_BASE, 'api/v1.0/classify/')
+
+        cnt = len(items)
+        print(cnt)
+        cls_data = []
+        while part_size * cur_part < cnt:
+            print(cur_part)
+
+            links_items = items[part_size * cur_part:part_size * (cur_part + 1)]
+            data = {
+                'links': links_items
+            }
+
+            try:
+                resp = requests.post(url, data=json.dumps(data))
+                resp_data = {}
+                for x in resp.json()['links']:
+                    for key, value in x.items():
+                        resp_data[key] = value
+            except (requests.exceptions.RequestException,
+                    requests.exceptions.Timeout,
+                    requests.exceptions.TooManyRedirects,
+                    simplejson.scanner.JSONDecodeError) as e:
+                resp_data = None
+
+            for x in links_items:
+                if resp_data is None:
+                    status = False
+                else:
+                    status = resp_data.get(x.get('link'), False)
+
+                cls_data.append({
+                    'link': x.get('link'),
+                    'moderator': x['data'].get('label'),
+                    'classificator': status,
+                })
+            cur_part += 1
+
+        out_path = os.path.abspath(os.path.normpath(options['out_path']))
+        if not os.path.isdir(os.path.dirname(out_path)):
+            os.makedirs(os.path.dirname(out_path))
+
+        with open(out_path, 'w') as fio:
+            fieldnames = cls_data[0].keys()
+            writer = csv.DictWriter(fio, fieldnames=fieldnames)
+            headers = dict((n, n) for n in fieldnames)
+            writer.writerow(headers)
+            for i in cls_data:
+                writer.writerow(i)
---
+++ digest/management/commands/cls_split_dataset.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import glob
+import json
+import math
+import os
+import random
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+from digest.management.commands.cls_create_dataset import save_dataset
+
+
+def load_data_from_folder(folder):
+    assert os.path.exists(folder)
+    result = []
+    for x in glob.glob('%s/*.json' % folder):
+        with open(x, 'r') as fio:
+            result.extend(json.load(fio)['links'])
+    return result
+
+
+class Command(BaseCommand):
+    help = u'Create dataset'
+
+    def add_arguments(self, parser):
+        parser.add_argument('cnt_parts', type=int)  # сколько частей
+        parser.add_argument('percent', type=int)  # сколько частей
+        parser.add_argument('items_folder', type=str)
+        parser.add_argument('add_folder', type=str)
+
+    def handle(self, *args, **options):
+        """
+        Основной метод - точка входа
+        """
+
+        items_data = []
+        items_data.extend(load_data_from_folder(options['add_folder']))
+        items_data.extend(load_data_from_folder(options['items_folder']))
+
+        random.shuffle(items_data)
+        items_cnt = len(items_data)
+
+        train_size = math.ceil(items_cnt * (options['percent'] / 100))
+        test_size = items_cnt - train_size
+        train_part_size = math.ceil(train_size / options['cnt_parts'])
+        test_part_size = math.ceil(test_size / options['cnt_parts'])
+
+        train_set = items_data[:train_size]
+        test_set = items_data[train_size:]
+
+        for part in range(options['cnt_parts']):
+            train_name = 'train_{0}_{1}.json'.format(train_part_size, part)
+            test_name = 'test_{0}_{1}.json'.format(test_part_size, part)
+            save_dataset(train_set[part * train_part_size: (part + 1) * train_part_size], train_name)
+            save_dataset(test_set[part * test_part_size: (part + 1) * test_part_size], test_name)
