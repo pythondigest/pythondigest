73417fce082f7fe88106d838fd3d74ba4be97598
Alexander Sapronov <sapronov.alexander92@gmail.com>
2016-05-27 10:09:26 +0600

    Update cls_create_datase.py Now script don't use many memory for import news

--- digest/management/commands/cls_create_dataset.py
+++ digest/management/commands/cls_create_dataset.py
@@ -5,7 +5,6 @@ import glob
 import json
 import math
 import os
-import random
 
 from django.conf import settings
 from django.core.management.base import BaseCommand
@@ -35,6 +34,20 @@ def save_dataset(data_items, name):
         json.dump(data, fio)
 
 
+def save_queryset_dataset(queryset, name):
+    if not queryset:
+        return
+    out_filepath = os.path.join(settings.DATASET_FOLDER, name)
+    with open(out_filepath, 'w') as fio:
+        fio.write('{"links": [\n')
+        items_cnt = queryset.count()
+        for i, item in enumerate(queryset):
+            fio.write(json.dumps(item.get_data4cls(status=True)))
+            if i != items_cnt - 1:
+                fio.write(',\n')
+        fio.write('\n]}')
+
+
 class Command(BaseCommand):
     help = 'Create dataset'
 
@@ -51,8 +64,7 @@ class Command(BaseCommand):
         for x in glob.glob('%s/*.json' % options['dataset_folder']):
             with open(x, 'r') as fio:
                 additional_data.extend(json.load(fio)['links'])
-
-        additional_data = additional_data
+        # TODO additional_data is off
 
         query = Q()
 
@@ -63,27 +75,48 @@ class Command(BaseCommand):
         for entry in urls:
             query = query | Q(link__contains=entry)
 
-        items = Item.objects.exclude(query).exclude(section=None).order_by('?')
+        active_news = Item.objects.filter(status='active') \
+            .exclude(section=None) \
+            .exclude(query)
+        links = active_news.all().values_list('link', flat=True).distinct()
+        non_active_news = Item.objects.exclude(link__in=links).exclude(query)
 
-        items_data = [x.get_data4cls(status=True) for x in items]
-        items_data.extend(additional_data)
-        random.shuffle(items_data)
-        items_cnt = len(items_data)
+        items_ids = list(active_news.values_list('id', flat=True))
+        items_ids.extend(non_active_news.values_list('id', flat=True))
+        items_ids = list(set(items_ids))
+
+        items = Item.objects.filter(id__in=items_ids).order_by('?')
+
+        items_cnt = items.count()
 
         train_size = math.ceil(items_cnt * (options['percent'] / 100))
         test_size = items_cnt - train_size
         train_part_size = math.ceil(train_size / options['cnt_parts'])
         test_part_size = math.ceil(test_size / options['cnt_parts'])
 
-        train_set = items_data[:train_size]
-        test_set = items_data[train_size:]
+        train_set = items[0: train_size]
+        test_set = items[train_size + 1:]
+        save_function = save_queryset_dataset
+
+        # items_data = [x.get_data4cls(status=True) for x in items]
+        # items_data.extend(additional_data)
+        # random.shuffle(items_data)
+        #
+        # train_set = items_data[:train_size]
+        # test_set = items_data[train_size:]
+        # save_function = save_dataset
 
         for part in range(options['cnt_parts']):
+            print("Create part {} (of {})".format(part, options['cnt_parts']))
             train_name = 'train_{0}_{1}.json'.format(train_part_size, part)
             test_name = 'test_{0}_{1}.json'.format(test_part_size, part)
-            save_dataset(
+
+            save_function(
                 train_set[part * train_part_size: (part + 1) * train_part_size],
-                train_name)
-            save_dataset(
+                train_name
+            )
+
+            save_function(
                 test_set[part * test_part_size: (part + 1) * test_part_size],
-                test_name)
+                test_name
+            )
