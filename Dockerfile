dc50bd505f9befbda0178491da158cd05887f0db
Alexander Sapronov <sapronov.alexander92@gmail.com>
2016-10-22 22:15:34 +0700

    Revert "Переход на bootstrap 4"

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM python:3.5.1
-ENV PYTHONUNBUFFERED 1
-RUN mkdir /pythondigest
-WORKDIR /pythondigest
-COPY requirements.txt /pythondigest/
-RUN pip install -r requirements.txt
-ADD . /pythondigest/
-EXPOSE 8099
--- conf/urls.py
+++ conf/urls.py
@@ -4,7 +4,6 @@ from controlcenter.views import controlcenter
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from conf.utils import likes_enable
 from digest.urls import urlpatterns as digest_url
@@ -47,6 +46,3 @@ if 'debug_toolbar' in settings.INSTALLED_APPS and settings.DEBUG:
     import debug_toolbar
 
     urlpatterns.append(url(r'^__debug__/', include(debug_toolbar.urls)))
-
-if settings.DEBUG:
-    urlpatterns += staticfiles_urlpatterns()
