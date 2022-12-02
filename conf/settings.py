4aae3c6440f00e24720b08be90911af4eefce6cf
axsapronov <a@sapronov.me>
2022-12-02 21:26:43 +0300

    CI: run docker-compose on server [WIP]

--- conf/settings.py
+++ conf/settings.py
@@ -34,7 +34,13 @@ DEBUG = env.bool("DEBUG", False)
 
 THUMBNAIL_DEBUG = False
 VERSION = (1, 0, 0)
-ALLOWED_HOSTS = ["pythondigest.ru", "pythondigest.sapronov.me", "127.0.0.1", "0.0.0.0"]
+ALLOWED_HOSTS = [
+    "pythondigest.ru",
+    "pythondigest.sapronov.me",
+    "188.120.227.123",
+    "127.0.0.1",
+    "0.0.0.0",
+]
 INTERNAL_IPS = ["127.0.0.1", "10.0.2.2"]
 
 INSTALLED_APPS = [
