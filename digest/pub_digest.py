b0da6b72bd5863583c80c7aff91626df12473426
axsapronov <a@sapronov.me>
2022-12-19 12:15:59 +0300

    Configure twitter timeout - 15 seconds

--- digest/pub_digest.py
+++ digest/pub_digest.py
@@ -26,7 +26,7 @@ def init_auth(consumer_key, consumer_secret, access_token, access_token_secret, 
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     proxy = get_https_proxy()
-    api = tweepy.API(auth_handler=auth, proxy=proxy)
+    api = tweepy.API(auth_handler=auth, proxy=proxy, timeout=15)
     return api
 
 
