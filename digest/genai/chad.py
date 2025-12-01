aa1b35cba3a79a19fe74c437d02021f6536063c3
axsapronov <a@sapronov.me>
2025-12-01 18:00:11 +0300

    🔧Migrate from poetry to uv #3

--- digest/genai/chad.py
+++ digest/genai/chad.py
@@ -23,8 +23,8 @@ from typing import Any, cast
 import requests
 from langchain_core.callbacks import CallbackManagerForLLMRun
 from langchain_core.language_models.llms import LLM
-from langchain_core.pydantic_v1 import SecretStr
 from langchain_core.utils import convert_to_secret_str, get_from_dict_or_env, pre_init
+from pydantic import SecretStr
 
 
 class Chad(LLM):
